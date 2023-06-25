85b7264c0fd9ec1dcf5390bf83034dbbcf747e3b
jurra <jose.urra86@gmail.com>
2023-06-25 17:42:00 +0200

    well_name should be name instead

--- models/well.py
+++ models/well.py
@@ -8,7 +8,7 @@ from .micro_core import MicroCore
 
 class Well(BaseModel):
     id: int = Field(description="The id of the well", example=1)
-    well_name: str = Field(
+    name: str = Field(
         ..., description="The name of the well", example="DEL-GT-01")
     cores: list = List[Core]
     cuttings: list = List[Cuttings]
